52c0924394aa2fa951986ece33de2219cc2a76ea
mainishanhoon <NishanKashyap@hotmail.com>
2024-11-14 00:50:19 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Faq from './sections/Faq';
+import Features from './sections/Features';
+import Header from './sections/Header';
+import Hero from './sections/Hero';
+import Pricing from './sections/Pricing';
+import Testimonials from './sections/Testimonials';
+import Download from './sections/Download';
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/constants/details.js
@@ -0,0 +1,22 @@
+export const details = [
+  {
+    id: '0',
+    icon: '/images/detail-1.webp',
+    title: 'AI automated video editing',
+  },
+  {
+    id: '1',
+    icon: '/images/detail-2.webp',
+    title: 'Collaborate with your team',
+  },
+  {
+    id: '2',
+    icon: '/images/detail-3.webp',
+    title: 'Ultra fast cloud-engine',
+  },
+  {
+    id: '3',
+    icon: '/images/detail-4.webp',
+    title: '24 / 7 Customer support',
+  },
+];
